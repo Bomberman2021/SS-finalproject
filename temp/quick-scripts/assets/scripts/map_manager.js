53efaddf099bd5a164343b9fad2ba4737c9953ea
Lohi <lohi3152@gmail.com>
2021-06-02 23:57:26 +0800

    add main scene

--- temp/quick-scripts/assets/scripts/map_manager.js
+++ temp/quick-scripts/assets/scripts/map_manager.js
@@ -26,7 +26,7 @@ var NewClass = /** @class */ (function (_super) {
         cc.director.getCollisionManager().enabled = true;
         // cc.director.getCollisionManager().enabledDrawBoundingBox = true;
         cc.director.getPhysicsManager().enabled = true;
-        cc.director.getPhysicsManager().debugDrawFlags = 1;
+        // cc.director.getPhysicsManager().debugDrawFlags = 1;
         cc.director.getPhysicsManager().gravity = cc.v2();
         this.initMap(this.node);
     };
