deb371d5258dad2de78b82919fe0b3a0f00214bf
Evan6684 <evan.hehehe@gmail.com>
2021-06-11 19:30:31 +0800

    added bomb layers on map

--- assets/tilesets/walls.tsx
+++ assets/tilesets/walls.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.0" name="bomb walls" tilewidth="64" tileheight="64" spacing="1" tilecount="24" columns="6">
- <image source="../sprites/map sprites/btiles.png" width="390" height="259"/>
+ <image source="../resources/map sprites/btiles.png" width="390" height="259"/>
 </tileset>
