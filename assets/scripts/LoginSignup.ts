d654dacf5f7407306fff5325125f534b04d64a42
Lohi <lohi3152@gmail.com>
2021-06-20 14:08:44 +0800

    add userAchievement database

--- assets/scripts/LoginSignup.ts
+++ assets/scripts/LoginSignup.ts
@@ -128,7 +128,25 @@ export default class LoginSignup extends cc.Component {
           level: 1,
           gameNum: 0,
           winNum: 0,
-          player2Mode: false,
+          userAchievement: {
+            0: 0,
+            1: 0,
+            2: 0,
+            3: 0,
+            4: 0,
+            5: 0,
+            6: 0,
+            7: 0,
+            8: 0,
+            9: 0,
+            10: 0,
+            11: 0,
+            12: 0,
+            13: 0,
+            14: 0,
+            15: 0,
+            16: 0,
+          },
         };
         firebase.database().ref(playersInfo).set(data);
         
